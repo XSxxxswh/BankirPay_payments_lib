7186d15379c412cbeb8eea17350893963b4d1a06
xxxswh <aa.danielyan0909@mail.ru>
2025-05-27 18:07:40 +0300

    kafka work performed

--- src/use_case/kafka.rs
+++ src/use_case/kafka.rs
@@ -65,64 +65,23 @@ pub(crate) async fn send_trader_change_balance_request(producer: &FutureProducer
         action_type: balance_action_type.into(),
         idempotent_key: Uuid::now_v7().to_string(),
     };
-    let buff = request.encode_to_vec();
-    for i in 0..3 {
-        if i > 0 {
-            warn!("Trader change balance kafka send retry attempt {}", i);
-            tokio::time::sleep(Duration::from_millis(100)).await;
-        }
-        let record = FutureRecord::to("trader_change_balance")
-            .key(&trader_id)
-            .payload(&buff);
-        match tokio::time::timeout(Duration::from_millis(300), producer.send(record, Timeout::Never)).await {
-            Ok(Ok(f)) => {
-                debug!("Sending trader change balance kafka send {:?}", f);
-                return Ok(());
-            },
-            Ok(Err((e, _))) => {
-                warn!(err=e.to_string(), "Error sending trader change balance kafka. Retrying...");
-                continue
-            },
-            Err(_) => {
-                warn!("Sending trader change balance kafka failed by timeout. Retrying...");
-                continue
-            }
-        }
+    let payload = request.encode_to_vec();
+    if bankirpay_lib::use_case::kafka::send_kafka_message(producer, "trader_change_balance", 
+                                                          trader_id.as_str(), &payload).await.is_err() {
+        error!(trader_id=trader_id, amount=?amount, action_type=?balance_action_type,
+            "Kafka send trader balance request error retry count exceeded");
+        return Err(InternalServerError)
     }
-    error!(trader_id=trader_id, amount=?amount, action_type=?balance_action_type, 
-        "Kafka send trader balance request error retry count exceeded");
-    Err(InternalServerError)
+    Ok(())
 }
 
 pub async fn send_payment_event_to_kafka(producer: &FutureProducer, payment: FullPayment) {
     let id = payment.id.clone();
     let payload = PaymentProto::from(payment).encode_to_vec();
-    for i in 0..3 {
-        if i > 0 {
-            warn!("Kafka retry send attempt {}", i);
-            tokio::time::sleep(Duration::from_millis(100)).await;
-        }
-        let record = FutureRecord::to("PAYMENT_EVENTS")
-            .key(&id)
-            .payload(&payload);
-        match tokio::time::timeout(Duration::from_millis(300), producer.send(record, Timeout::Never)).await 
-        {
-            Ok(Ok(_)) => {
-                debug!("Sending payment event to kafka kafka send");
-                return;
-            },
-            Ok(Err((e, _))) => {
-                warn!(err=e.to_string(), "Error sending payment event to kafka kafka. Retrying...");
-                continue
-            },
-            Err(_) => {
-                warn!("Sending payment event to kafka timeout. Retrying...");
-                continue
-            }
-        }
-        
+    if bankirpay_lib::use_case::kafka::send_kafka_message(producer, "PAYMENT_EVENTS", id.as_str(),  payload.as_slice()).await.is_err() 
+    {
+        error!(payment_id=id, "Kafka error send event to kafka");
     }
-    error!(payment_id=id, "Kafka error send event to kafka");
 }
 
 async fn handle_kafka_notification_event(state: Arc<models::State>, message: &BorrowedMessage<'_>)
@@ -135,14 +94,14 @@ async fn handle_kafka_notification_event(state: Arc<models::State>, message: &Bo
                         warn!(bank_id=notification.id, "payment by notification not found");
                     }else {
                         error!(err=e.to_string(), "Error closing payment");
-                        bankirpay_lib::use_case::kafka::send_kafka_message(&state.kafka_producer, "HANDLE_NOTIFICATION_ERRORS",
+                        let _ = bankirpay_lib::use_case::kafka::send_kafka_message(&state.kafka_producer, "HANDLE_NOTIFICATION_ERRORS",
                                            notification.id.as_str(), payload).await;
                     }
                 }
             },
             Err(e) => {
                 error!(err=e.to_string(), "Error decode payload from kafka");
-                bankirpay_lib::use_case::kafka::send_kafka_message(&state.kafka_producer, "HANDLE_NOTIFICATION_ERRORS",
+                let _ = bankirpay_lib::use_case::kafka::send_kafka_message(&state.kafka_producer, "HANDLE_NOTIFICATION_ERRORS",
                                    Uuid::now_v7().to_string().as_str(), payload).await;
             }
         };
